1ee8ab7f0a36b9164c798d7a9c6892b202b6d54c
Sebastian Gionfriddo <97997717+SebGio@users.noreply.github.com>
2025-03-16 18:16:50 -0600

    finished code

--- coordinate.cpp
+++ coordinate.cpp
@@ -1,88 +1,95 @@
 #include "coordinate.h"
 #include <math.h>
-#include <iostream.h>
-#include <stdlib.h>
+#include <iostream>
+
 
 void add_coordinate(Coordinate *list_end, float x, float y)
 {
-	Coordinate * newCoord = (Coordinate)malloc(sizeof(coordinate));
+	Coordinate * newCoord = new Coordinate;
 	newCoord->x = x;
 	newCoord->y = y;
-	//newCoord->id = list_end->id + 1;
-	newCoord->next = null;
+	newCoord->next = NULL;
+
+	newCoord->coord_id = list_end->coord_id + 1;
 	newCoord->previous = list_end;
 	list_end->next = newCoord;
+
 }
 void forward_display(Coordinate *list_beginning)
 {
-
-	while(list_beginning->next != null)
+	std::cout << "Forward display:" << std::endl;
+	//while(list_beginning->next != NULL)
+	while (list_beginning != NULL)
 	{
 		std::cout << "ID: " << list_beginning->coord_id << " X: " << list_beginning->x << " Y: " << list_beginning->y << std::endl;
 		list_beginning = list_beginning->next;
 	}
-	//last value will get skipped, make sure it still gets output
-	//std::cout << "ID: " << list_beginning->coord_id << " X: " << list_beginning->x << " Y: " << list_beginning->y << std::endl;
-	//for (int i = 0; i <  list_length(list_beginning); ++i)
-	//{
-		//std::cout << "ID: " << (list_beginning + i)->coord_id << " X: " << (list_beginning + i)->x << " Y: " << (list_beginning + i)->y << std::endl;
-	//}
 }
 void backward_display(Coordinate *list_end)
 {
-	while(list_end->previous != null)
+	std::cout << "Backward display:" << std::endl;
+	//while(list_end->previous != NULL)
+	while(list_end != NULL)
 	{
 		std::cout << "ID: " << list_end->coord_id << " X: " << list_end->x << " Y: " << list_end->y << std::endl;
 		list_end = list_end->previous;
 	}
-	//last value will get skipped, make sure it still gets output
-	//std::cout << "ID: " << list_end->coord_id << " X: " << list_end->x << " Y: " << list_end->y << std::endl;
-
-	//for (int i =  list_length(list_beginning); i > 0;  --i)
-	//{
-	//	std::cout << "ID: " << (list_beginning + i)->coord_id << " X: " << (list_beginning + i)->x << " Y: " << (list_beginning + i)->y << std::endl;
-	//}
-
 }
 void delete_coordinate(Coordinate *list_beginning, int coord_id_to_delete)
 {
 	Coordinate *temp = list_beginning;
-	if (temp != null)
+	if (temp != NULL)
 	{
 		while (temp->coord_id != coord_id_to_delete)
 		{
 			temp = temp->next;
+			if (temp == NULL)
+			{
+				std::cout << "Cannot delete coordinate " << coord_id_to_delete << ": Invalid ID" << std::endl;
+				return;
+			}
 		}
-		if (temp == null)
-
+		//check if value is at beginning or end of list, and update values
+		temp->next != NULL ? temp->next->previous = temp->previous : temp->previous->next = NULL;
+		temp->previous != NULL ? temp->previous->next = temp->next : temp->next->previous = NULL;
+		
+		delete temp;
 	}
 	else
+	{
+		std::cout << "List is empty." << std::endl;
 	}
-		std::cout << "List is empty." << endl;
-	}
+	
 }
 int list_length(Coordinate *list_beginning)
 {
 	int length = 0;
-	while (list_beginning->next != null)
+	while (list_beginning != NULL)
 	{
 		++length;
+		list_beginning = list_beginning->next;
 	}
 	return length;
 }
 void closest_to(Coordinate *list_beginning, float x, float y)
 {
-	float distance = 0;
-	while(list_beginning->next != null)
+	float minDistance = FLT_MAX;
+	Coordinate* closeCoordinate = list_beginning;
+	while(list_beginning != NULL)
 	{
 		//pythagorean theorem
-		float xDistance = abs(list_beginning->x) + abs(x);
-		float yDistance = abs(list_beginning->y) + abs(y);
+		float xDistance = abs(list_beginning->x - x);
+		float yDistance = abs(list_beginning->y - y);
 		float newDistance = sqrt(pow(xDistance, 2) + pow(yDistance, 2));
-		if (distance > newDistance)
+
+		if (minDistance > newDistance)
 		{
-			distance = newDistance;	
+			minDistance = newDistance;
+			closeCoordinate = list_beginning;
 		}
+		list_beginning = list_beginning->next;
 	}
+	//output result
+	std::cout << "Closest coordinate to " << x << ", " << y << ":	ID: " << closeCoordinate->coord_id << " X: " << closeCoordinate->x << " Y: " << closeCoordinate->y << std::endl;
 	
 }
--- main.cpp
+++ main.cpp
@@ -1,8 +1,64 @@
 #include "coordinate.h"
+#include <iostream>
+#include <string>
+#include <stdlib.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+	//make sure number of coordinates is specified
+	if (argc == 1)
+	{
+		std::cout << "Please specify a number of coordinates to create when running the program" << std::endl;
+		return 1;
+	}
+	
 
+	//seed random
+	srand(time(NULL));
+
+	int numCoords = std::stoi(argv[1]);
+
+	//create initial element
+	Coordinate* head = new Coordinate;
+	head->coord_id = 0;
+	head->previous = NULL;
+
+	Coordinate* tail = head;
+	//fill the list
+	for (int i = 0; i < numCoords; ++i)
+	{
+		add_coordinate(tail, (float)rand() / 10, (float)rand() / 10);
+		tail = tail->next;
+	}
+
+	//delete initial value so the length is what the user requested.
+	head = head->next;
+	delete_coordinate(head->previous, 0);
+
+	std::cout << "Length before delete: " << list_length(head) << std::endl;
+	forward_display(head);
+	backward_display(tail);
+	
+	//delete some elements
+	delete_coordinate(head, 4);
+	tail = tail->previous;
+	delete_coordinate(head, 10);
+
+	std::cout << "Length after delete: " << list_length(head) << std::endl;
+	list_length(head);
+	forward_display(head);
+	backward_display(tail);
+	
+
+	closest_to(head, (float)rand() / 10, (float)rand() / 10);
+
+	//clear memory
+	while (head != NULL)
+	{
+		Coordinate* temp = head->next;
+		delete head;
+		head = temp;
+	}
 
 return 0;
 }
